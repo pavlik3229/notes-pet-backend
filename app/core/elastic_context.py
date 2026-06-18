71c091ff99d2bba1009a7347e9cf40d7f67e0125
pavlik3229 <147932271+pavlik3229@users.noreply.github.com>
2026-06-18 17:14:15 +0300

    feat: safe function to generate es connection

--- app/core/elastic_context.py
+++ app/core/elastic_context.py
@@ -1,9 +1,15 @@
-from elasticsearch import AsyncElasticsearch
+from typing import AsyncGenerator
 
+from elasticsearch import AsyncElasticsearch
 from app.core.config import config
 
-es_client = AsyncElasticsearch(config.ELASTICSEARCH_URL)
 
-
-async def get_es():
-    return es_client
+async def get_es() -> AsyncGenerator[AsyncElasticsearch, None]:
+    """
+    Dependency function to provide an instance of AsyncElasticsearch for interacting with Elasticsearch.
+    """
+    es = AsyncElasticsearch(config.ELASTICSEARCH_URL)
+    try:
+        yield es
+    finally:
+        await es.close()
